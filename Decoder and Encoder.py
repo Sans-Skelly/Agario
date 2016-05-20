23aa68fa3866033e75b54694d5b13588d3234745
whitelightning12 <akarner@bell.net>
2016-05-19 21:30:20 -0400

    Updated to correct file.

--- Decoder and Encoder.py
+++ Decoder and Encoder.py
@@ -1,12 +1,18 @@
-def string_decoder (recieved_string):
-    
+def recieving_decoder(recieved_string):
+    """ (lst)(lst) -> (str)
+    Decodes food info and player info from string into list.
+    food_info format:    [[x,y,m],[x,y,m]]
+    players_info format: [[[[x,y,m],[x,y,m]],(r,g,b),name],[[[x,y,m],[x,y,m]],(r,g,b),name]]
+    String format:       'x,y,m/x,y,m|x,y,m:x,y,z;r,g,b;name/x,y,z:x,y,z;r,g,b;name'
+    """
 
     categories = recieved_string.split('|')
     food_info = categories[0]
-    player_info = categories[1]
+    players_info = categories[1]
     
     food_info = food_info.split('/')
-    player_info = player_info.split('/')
+    players_info = players_info.split('/')
+    
 # FOOD Decoding
     for i in range(len(food_info)):
         food_info[i] = food_info[i].split(',')
@@ -14,19 +20,50 @@ def string_decoder (recieved_string):
             food_info[i][u] = int(food_info[i][u])
     
 #Player Decoding
-    for i in range(len(player_info)):
-        player_info[i] = player_info[i].split(';')
-        player_info[i][0] = player_info[i][0].split(':')
-        player_info[i][1] = player_info[i][1].split(',')
-        for e in range(len(player_info[i][1])):
-            player_info[i][1][e] = int(player_info[i][1][e])
-        player_info[i][1] = tuple(player_info[i][1])
-    for i in range(len(player_info)):
-        for e in range(len(player_info[i][0])):
-            player_info[i][0][e] = player_info[i][0][e].split(',')
-            for u in range(len(player_info[i][0][e])):
-                player_info[i][0][e][u] = int(player_info[i][0][e][u])
+    for i in range(len(players_info)):
+        players_info[i] = players_info[i].split(';')
+        players_info[i][0] = players_info[i][0].split(':')
+        players_info[i][1] = players_info[i][1].split(',')
+        for e in range(len(players_info[i][1])):
+            players_info[i][1][e] = int(players_info[i][1][e])
+        players_info[i][1] = tuple(players_info[i][1])
+    for i in range(len(players_info)):
+        for e in range(len(players_info[i][0])):
+            players_info[i][0][e] = players_info[i][0][e].split(',')
+            for u in range(len(players_info[i][0][e])):
+                players_info[i][0][e][u] = int(players_info[i][0][e][u])
 
-    return food_info,player_info
+    return food_info,players_info
 
-print string_decoder("200,400,0/100,350,1|300,245,50:250,245,40;255,255,255/600,150,200;255,255,255")
+def sending_encoder(food_info,players_info):
+    """ (lst)(lst) -> (str)
+    Encodes food info and player info lists into sendable string.
+    food_info format:    [[x,y,m],[x,y,m]]
+    players_info format: [[[[x,y,m],[x,y,m]],(r,g,b),name],[[[x,y,m],[x,y,m]],(r,g,b),name]]
+    String format:       'x,y,m/x,y,m|x,y,m:x,y,z;r,g,b;name/x,y,z:x,y,z;r,g,b;name'
+    """
+    
+    #Food Encoding
+    for i in range(len(food_info)):
+        for e in range(len(food_info[i])):
+            food_info[i][e] = str(food_info[i][e])
+        food_info[i] = ','.join(food_info[i])
+    food_info = '/'.join(food_info)
+
+    #Player Encoding
+    for i in range(len(players_info)):
+        for e in range(len(players_info[i][0])):
+            for u in range(len(players_info[i][0][e])):
+                players_info[i][0][e][u] = str(players_info[i][0][e][u])
+            players_info[i][0][e] = ','.join(players_info[i][0][e])
+    for i in range(len(players_info)):
+        players_info[i][1] = list(players_info[i][1]) #Change colour tuple to list
+        print players_info[1]
+        for e in range(len(players_info[i][1])):
+            players_info[i][1][e] = str(players_info[i][1][e])
+        players_info[i][1] = ','.join(players_info[i][1])
+        players_info[i][0] = ':'.join(players_info[i][0])
+        players_info[i] = ';'.join(players_info[i])
+    players_info = '/'.join(players_info)
+           
+    return food_info+'|'+players_info 
