e8f715f68444b00ad0aec38871b31cd770341ab8
Steven Kovacs-Snider <stevenks123@gmail.com>
2016-06-02 10:20:29 -0400

    Update Food.py

--- Pygar.io/Food.py
+++ Pygar.io/Food.py
@@ -9,18 +9,17 @@ import random
 import pygame
 
 class Food:
-    def __init__(self,surface,screenHeight,screenWidth):
+    def __init__(self,screenHeight,screenWidth):
         """
         This function assigns basic constants and variables too any and all food objects
         """
         self.x = random.randint(25,screenWidth-25)
         self.y = random.randint(25,screenHeight-25)
         self.color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
-        self.surface = surface
              
-    def render(self,camera):
+    def render(self,camera,surface):
         """ (object) --> (None) """
-        pygame.draw.circle(self.surface,self.color,(int(self.x*camera.zoom+camera.x),int(self.y*camera.zoom+camera.y)),int(camera.zoom*4))
+        pygame.draw.circle(surface,self.color,(int(self.x*camera.zoom+camera.x),int(self.y*camera.zoom+camera.y)),int(camera.zoom*4))
             
 def spawn_food(food_list,num_of_food_elements,surface,screenHeight,screenWidth):
     """ (list),(int),(object),(int),(int) --> (list)
