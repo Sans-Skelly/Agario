095f786db1cb2b64c419a56dc4b0854338341d80
Elarmer <elarmer@hotmail.ca>
2016-06-02 10:28:34 -0400

    Update labels_and_text.py

--- Pygar.io/labels_and_text.py
+++ Pygar.io/labels_and_text.py
@@ -3,6 +3,9 @@ pygame.font.init()
 myfont = pygame.font.SysFont("monospace",15)
 
 def render_score_label(screen_width,screen_height,score,surface):
+    """(int),(int),(int),(object) ---> (none)
+    Renders your personal score(mass) in the top left corner of your screen.
+    """
     x = screen_width / 40
     y = screen_height / 40
     score_label = myfont.render("Score: " + (str(score)),1,(0,0,0))
