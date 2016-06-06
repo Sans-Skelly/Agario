bbea48ed29c065bbb4371cddf1952eb03c906134
sagniksom <sagniksom99@gmail.com>
2016-06-06 10:33:18 -0400

    Update Camera.py

--- Pygar.io/Camera.py
+++ Pygar.io/Camera.py
@@ -1,6 +1,10 @@
 import pygame
 class Camera:
+    """ A class that deals with visual scaling of objects and determines the local coordintates of the objects.
     def __init__(self, screenWidth, screenHeight):
+        """ (object, int, int) --> (None)
+        This is a descriptor method for the class Camera
+        """
         self.x = 0
         self.y = 0
         self.Xspeed = 0
@@ -10,6 +14,9 @@ class Camera:
         self.zoom = 10.0
 
     def centre(self,player,screenWidth,screenHeight):
+        """ (object,object,int,int) --> (None)
+        Centres the player on the screen
+        """
         self.x = (screenWidth/2)-(player.x*self.zoom)
         self.y = (screenHeight/2)-(player.y*self.zoom)
             
