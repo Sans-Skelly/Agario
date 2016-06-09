fddcb6538ba039b057dd744401e02364383dc697
sagniksom <sagniksom99@gmail.com>
2016-06-09 11:43:16 -0400

    Update Player.py

--- Pygar.io/Player.py
+++ Pygar.io/Player.py
@@ -127,6 +127,22 @@ class Player:
                     food_blob_list.append(food_blob)
                     self.mass = self.mass-food_blob_size
                     self.cameraValue -= 5*float(food_blob_size)/self.mass
+    
+    def explode2(self,camera,viruses,food_blob_list):
+         for item in viruses:
+            if distance(self.x,self.y,item.x,item.y) <= self.mass/3 and self.mass >= 100:
+                food_blob_size = (self.mass*0.7)/8
+                segments = points_on_circumfrence(self.mass/3,7)
+                angles = [(2*math.pi/n*x)*r for x in xrange(0,8)]
+                for i in range(8):
+                    x,y = segments[i]
+                    x += self.x
+                    y += self.y
+                    food_blob = food_blobs(self.surface,x,y,self.mass,camera,self.color)
+                    food_blob.angle = angles[i]
+                    food_blob_list.append(food_blob)
+                    self.mass = self.mass-food_blob_size
+                    self.cameraValue -= 5*float(food_blob_size)/self.mass
                 
     def split(self,surface,screenHeight,screenWidth):
         """ (object),(int),(int) ---> (None)
