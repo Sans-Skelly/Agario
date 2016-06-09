eb1e894d1af35aef41753ecfe0666d998fa5060b
sagniksom <sagniksom99@gmail.com>
2016-06-09 10:41:37 -0400

    Update Virus.py

--- Pygar.io/Virus.py
+++ Pygar.io/Virus.py
@@ -2,6 +2,8 @@ import pygame
 import random
 class Virus(object):
     def __init__(self,surface,screenHeight,screenWidth):
+        """ (object),(object),(int),(int) ---> (None) 
+        """
         self.x = random.randint(25,screenHeight - 25)
         self.y = random.randint(25,screenWidth - 25)
         self.mass = 100
@@ -10,10 +12,14 @@ class Virus(object):
         self.surface = surface
         
     def render(self,camera):
+        """ (object),(object) ---> (None) 
+        """
         x = int(self.x*camera.zoom+camera.x)
         y = int(self.y*camera.zoom+camera.y)
         pygame.draw.circle(self.surface,self.color,(x,y),int(self.mass/2*camera.zoom), 0)
         pygame.draw.circle(self.surface,self.borderColor,(x,y),int(self.mass/2*camera.zoom),15)
 
     def update(self,camera):
+        """ (object),(object) ---> (None) 
+        """
         self.render(camera)
