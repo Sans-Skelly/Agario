50d49a4fe4733981a428b04b67fd762f45e6dc76
sagniksom <sagniksom99@gmail.com>
2016-06-10 10:43:30 -0400

    Update Segment.py

--- Pygar.io/Segment.py
+++ Pygar.io/Segment.py
@@ -22,18 +22,20 @@ class Segment(food_blobs):
             segments.remove(self)
             
     def explode(self,camera,viruses,food_blob_list):
-        food_blob_size = (self.mass*0.7)/8
-        segments = points_on_circumfrence(self.mass/3,7)
-        angles = [(2*math.pi/7*x)*(self.mass/3) for x in xrange(0,8)]
-        for i in range(8):
-            x,y = segments[i]
-            x += self.x
-            y += self.y
-            food_blob = food_blobs(self.surface,x,y,self.mass,camera,self.color)
-            food_blob.angle = angles[i]
-            food_blob_list.append(food_blob)
-            self.mass = self.mass-food_blob_size
-            self.cameraValue -= 5*float(food_blob_size)/self.mass
+        for item in viruses:
+            if distance(self.x,self.y,item.x,item.y) <= self.mass/3 and self.mass >= 100:
+                food_blob_size = (self.mass*0.7)/8
+                segments = points_on_circumfrence(self.mass/3,7)
+                angles = [(2*math.pi/7*x)*(self.mass/3) for x in xrange(0,8)]
+                for i in range(8):
+                    x,y = segments[i]
+                    x += self.x
+                    y += self.y
+                    food_blob = food_blobs(self.surface,x,y,self.mass,camera,self.color)
+                    food_blob.angle = angles[i]
+                    food_blob_list.append(food_blob)
+                    self.mass = self.mass-food_blob_size
+                    self.cameraValue -= 5*float(food_blob_size)/self.mass
 
     def render(self,camera):
         pygame.draw.circle(self.surface,self.color,(int(self.x*camera.zoom+camera.x),int(self.y*camera.zoom+camera.y)),int(camera.zoom*self.mass/3),0)
@@ -43,6 +45,7 @@ class Segment(food_blobs):
         self.move()
         self.collision_detection(item_list, item_value, required_mass, camera)
         self.fuse(player,segments)
+        self.explode(self,camera,viruses,food_blob_list)
         
 def split(segments,surface,player,camera):
     if player.mass >= 32:
