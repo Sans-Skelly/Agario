83078f157343e2982ec585df6cab57c1971838f9
sagniksom <sagniksom99@gmail.com>
2016-06-20 10:16:45 -0400

    Update Virus.py

--- Pygar.io/Virus.py
+++ Pygar.io/Virus.py
@@ -2,6 +2,9 @@ import pygame
 import random
 class Virus(object):
     def __init__(self,surface,playfieldWidth,playfieldHeight):
+        """(object),(object),(int),(int) ---> (None)
+        Construtor Method for Viruses. 
+        """
         self.x = random.randint(25,playfieldWidth)
         self.y = random.randint(25,playfieldHeight)
         self.mass = 100.111
@@ -10,10 +13,16 @@ class Virus(object):
         self.surface = surface
     
     def render(self,camera):
+        """(object),(object) ---> (None)
+        Construtor Method for Viruses. 
+        """ 
         x = int(self.x*camera.zoom+camera.x)
         y = int(self.y*camera.zoom+camera.y)
         pygame.draw.circle(self.surface,self.color,(x,y),int(self.mass/3*camera.zoom), 0)
         pygame.draw.circle(self.surface,self.borderColor,(x,y),int(self.mass/3*camera.zoom),5)
         
     def update(self,camera):
+        """(object),(object) ---> (None)
+        Updates Viruses.
+        """
         self.render(camera)
